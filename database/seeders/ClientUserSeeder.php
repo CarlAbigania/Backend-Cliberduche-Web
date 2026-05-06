99fc0cc68e5b458766177e3af61343f6b6eff9ca
CarlAbigania <abiganiacarlo687@gmail.com>
2026-05-06 17:34:11 +0800

    .

--- database/seeders/ClientUserSeeder.php
+++ database/seeders/ClientUserSeeder.php
@@ -16,16 +16,18 @@ class ClientUserSeeder extends Seeder
         for ($i = 1; $i <= 10; $i++) {
             $status = $i <= 3 ? 'pending' : 'approved';
 
-            User::create([
-                'firstname' => 'Client' . $i,
-                'lastname' => 'User',
-                'email' => 'client' . $i . '@gmail.com',
-                'contact_number' => '0912345678' . $i,
-                'password' => Hash::make('password123'),
-                'role' => 'client',
-                'account_status' => $status,
-                'is_active' => true,
-            ]);
+            User::updateOrCreate(
+                ['email' => 'client' . $i . '@gmail.com'],
+                [
+                    'firstname' => 'Client' . $i,
+                    'lastname' => 'User',
+                    'contact_number' => '0912345678' . $i,
+                    'password' => Hash::make('password123'),
+                    'role' => 'client',
+                    'account_status' => $status,
+                    'is_active' => true,
+                ]
+            );
         }
     }
 }
